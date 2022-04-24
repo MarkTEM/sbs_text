be35b7145fadfb2a024127aa459f876f1e8332c1
MarkTEM <mark.gucella@mail.ru>
2022-04-25 01:56:51 +0300

    Update main.js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// preloader();
+
+let preloader = document.querySelector('.preloader');
+
+window.addEventListener('load', () => {
+  preloader.classList.add('hide');
+  setTimeout(() => {
+    preloader.remove()
+  },600)
+});
